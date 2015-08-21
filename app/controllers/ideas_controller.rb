5d4b1607b7abe636a3031cc426384cf080c23ea6
pafer2004 <pafer2004@gmail.com>
2015-08-21 16:02:00 -0500

    primer commit

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,95 @@
+class IdeasController < ApplicationController
+  before_action :set_idea, only: [:show, :edit, :update, :destroy]
+
+  # GET /ideas
+  # GET /ideas.json
+  def index
+    @ideas = Idea.all
+  end
+
+  # GET /ideas/1
+  # GET /ideas/1.json
+  def show
+    commontator_thread_show(@idea)
+
+  end
+
+  # GET /ideas/new
+  def new
+    @idea = Idea.new
+  end
+
+
+
+  def upvote
+  @idea = Idea.find(params[:id])
+  @idea.upvote_by current_user
+  redirect_to ideas_path
+  end
+  def downvote
+  @idea = Idea.find(params[:id])
+  @idea.downvote_by current_user
+  redirect_to ideas_path
+  end
+
+
+  # GET /ideas/1/edit
+  def edit
+    @idea = current_user.ideas.find_by_id(params[:id])
+    if @idea.nil?
+    redirect_to ideas_path, flash: {alert: 'No existe la idea que desea modificar'}
+    end
+  end
+
+  # POST /ideas
+  # POST /ideas.json
+  def create
+    @idea = Idea.new(idea_params)
+    @idea.user = current_user
+    respond_to do |format|
+      if @idea.save
+        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.json { render :show, status: :created, location: @idea }
+      else
+        format.html { render :new }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /ideas/1
+  # PATCH/PUT /ideas/1.json
+  def update
+    @idea = current_user.ideas.find(params[:id])
+    respond_to do |format|
+      if @idea.update(idea_params)
+        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        format.json { render :show, status: :ok, location: @idea }
+      else
+        format.html { render :edit }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /ideas/1
+  # DELETE /ideas/1.json
+  def destroy
+    @idea.destroy
+    respond_to do |format|
+      format.html { redirect_to ideas_url, notice: 'Idea was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_idea
+      @idea = Idea.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def idea_params
+      params.require(:idea).permit(:name, :description, :picture)
+    end
+end
---
+++ app/models/idea.rb
@@ -0,0 +1,7 @@
+class Idea < ActiveRecord::Base
+  mount_uploader :picture, PictureUploader
+  belongs_to :user
+  acts_as_commontable
+  acts_as_votable
+
+end
